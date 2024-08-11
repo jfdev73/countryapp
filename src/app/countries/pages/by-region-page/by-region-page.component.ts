4f0ebf55bfa6bf9bb18a110bfd3d0b779e329b67
jfdev <jfdev73@gmail.com>
2024-08-10 22:21:30 -0600

    Search By Region Optimization

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country';
 
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
+
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -9,10 +11,14 @@ import { Country } from '../../interfaces/country';
 })
 export class ByRegionPageComponent {
   countries: Country[] = [];
+  regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+
+  selectedRegion?: Region;
 
   constructor(private countriesService: CountriesService) {}
 
-  searchByRegion(term: string) {
+  searchByRegion(term: Region) {
+    this.selectedRegion = term;
     this.countriesService.searchRegion(term).subscribe((countries) => {
       this.countries = countries;
     });
